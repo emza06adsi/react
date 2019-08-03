4798b768981dc3defd984b3eb7c5cc0b03776a2b
emza06adsi <52024171+emza06adsi@users.noreply.github.com>
2019-08-03 09:23:30 -0500

    Update index.js

--- tres/src/index.js
+++ tres/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-constructor(props){
-    super(props);
-    this.state={ value:null};
-}
-
     render() {
       return (
-        <button className="square"
-         onClick={()=>this.props.onClick()}>
+        <button
+         className="square"
+         onClick={()=>this.props.onClick()}
+         >
           {this.props.value}
         </button>
       );
@@ -32,8 +29,12 @@ constructor(props){
       }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}
-      onClick={()=>this.handleClick(i)} />;
+      return( 
+      <Square
+       value={this.state.squares[i]}
+       onClick={()=>this.handleClick(i)}
+         />
+       );
     }
   
     render() {
